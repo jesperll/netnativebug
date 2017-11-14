684fa01514fe1c4d44ede17280ef44cb309b1e7f
Jesper Larsen-Ledet <jll@trackman.dk>
2017-11-14 09:47:40 +0100

    Add project files.

---
+++ App2/Something.cs
@@ -0,0 +1,6 @@
+﻿namespace App2
+{
+    // these two lines will result in the .Net Native compiler failing
+    interface ISomething<T1, T2> { }
+    class Something<T> : ISomething<T[], T[][]> { }
+}
